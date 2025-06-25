88f76bbe60b510e945ab5de6c6cd29568905795a
SneakyDonut <jairampranav@gmail.com>
2025-06-25 22:20:18 +0200

    a1

--- app/page.tsx
+++ app/page.tsx
@@ -465,7 +465,7 @@ export default function Component() {
           </div>
 
           <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-            <p>&copy; 2025 Illovo Nursery. All rights reserved. Designed by Prestige Sites.</p>
+            <p>&copy; 2025 Illovo Nursery. Designed by Prestige Sites. All rights reserved.</p>
           </div>
         </div>
       </footer>
